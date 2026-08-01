7eb7557b8646e20346874f9e20fa952576043f6f
Chaitany Belambkar <chaitanybelambkar9689@gmail.com>
2026-08-01 21:31:50 +0530

    Add files via upload

---
+++ Programming-Practice/program10.c
@@ -0,0 +1,86 @@
+/*
+step1:understand the problem statement
+step2:write a algorithm
+step3:decide the program language
+step4:Write the program
+step5:test the program
+*/
+/*
+Algorithm
+
+START
+    Accept first no as no1
+    Accept second no as no2
+    if input is negative then it convert into positive
+    perform Addition of no1 and no2
+    Display the Addition on screen
+STOP
+
+*/
+//////////////////////////////////////////////////////////////////////////
+//
+//   Required Header files
+//
+/////////////////////////////////////////////////////////////////////////
+#include<stdio.h>
+//////////////////////////////////////////////////////////////////////////
+//
+// Function Name: AdditionTwoNumbers 
+// Description:   It is used to perform Addition
+// Input:         Float,Float
+// Output:        Float
+// Author:        Chaitany Dilip Belambkar
+// Date:          9/10/2025
+//////////////////////////////////////////////////////////////////////////
+
+
+int AdditionTwoNumbers(float fNo1,float fNo2) 
+{
+    float fSum = 0.0f;
+    
+    //Updator
+    if(fNo1 < 0.0f)
+    {
+        fNo1 = -fNo1;
+    }
+    //Updator
+    if(fNo2 < 0.0f)
+    {
+        fNo2 = -fNo2;
+    }
+
+    fSum =fNo1+fNo2;   //Bussiness logic
+    return fSum;
+
+}
+
+//////////////////////////////////////////////////////////////////////////
+//
+//  Entry point function for the application
+//
+//////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+  float fValue1 = 0 ,fValue2 = 0 ,fRet = 0;
+
+  printf("Enter first number:\n");
+  scanf("%f",&fValue1);
+
+  printf("Enter Second number:\n");
+  scanf("%f",&fValue2);
+  
+  fRet = AdditionTwoNumbers(fValue1,fValue2);
+
+  printf("Addition is:%f\n",fRet);
+  return 0;
+} 
+
+//////////////////////////////////////////////////////////////////////////
+// Testcase successfully Handeled by the Application
+// Input1 : 10.5  Input2 : 3.2  Output : 13.7
+// Input1 : -10.5  Input2 : 3.2  Output : 13.7
+// Input1 : 10.5  Input2 : -3.2  Output : 13.7
+// Input1 : -10.5  Input2 : -3.2  Output : 13.7
+// Input1 : +10.5  Input2 : - 3.2  Output : 13.7
+//////////////////////////////////////////////////////////////////////////
