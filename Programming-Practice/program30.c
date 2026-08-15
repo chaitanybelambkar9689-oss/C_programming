508e696c230cabc5ae23c0f14a3310bb16f6621b
Chaitany Belambkar <chaitanybelambkar9689@gmail.com>
2026-08-15 21:48:20 +0530

    Add files via upload

---
+++ Programming-Practice/program30.c
@@ -0,0 +1,19 @@
+// 1  2 3  4 5
+#include<stdio.h>
+
+void Display()
+{   
+ int iCnt = 1;
+ while(iCnt <=5)
+ {
+    printf("%d\t",iCnt);
+    iCnt++;
+ }
+ printf("\n");
+}
+int main()
+{
+    Display();
+
+    return 0;
+}
