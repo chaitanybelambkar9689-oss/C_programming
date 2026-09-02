a74e998720e464577c304e99e55f61d417227836
Chaitany Belambkar <chaitanybelambkar9689@gmail.com>
2026-09-02 19:28:43 +0530

    Add files via upload

---
+++ Programming-Practice/program48.c
@@ -0,0 +1,36 @@
+
+#include<stdio.h>
+#include<stdbool.h>
+
+
+void DisplayFactors(int iNo)
+{
+  int iCnt = 0;
+  if (iNo < 0)
+  {
+    iNo = -iNo;
+  }
+  for(iCnt = 1;iCnt<= iNo;iCnt++)
+  {
+  if( (iNo % iCnt) == 0)
+  {
+    printf("%d\t",iCnt);
+
+  }
+   
+  }
+}
+
+
+int main()
+{
+    int iValue = 0;
+   
+    printf("Enter number:\n");
+    scanf("%d",&iValue);
+
+    DisplayFactors(iValue);
+
+   
+    return 0;
+}
