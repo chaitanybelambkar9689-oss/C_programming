e3a958897f91180fded82a8780884ea41414d7bd
Chaitany Belambkar <chaitanybelambkar9689@gmail.com>
2026-09-08 21:17:23 +0530

    Add files via upload

---
+++ Programming-Practice/program54.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPrime(int iNo)
+{
+    int iCnt = 0,iFrequency = 0;
+    
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+
+    }
+    for(iCnt=2;iCnt <= (iNo/2);iCnt++)
+    {  
+        if((iNo %iCnt) ==0)
+        {
+            iFrequency++;
+        }
+    }
+    if(iFrequency == 0) //no facotors
+    {
+        return true;
+    }
+    else  //Atleast one factor
+    {
+        return false;
+    }
+ 
+}
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter the number:\n");
+    scanf("%d",&iValue);
+    bRet =CheckPrime(iValue);
+    
+    if(bRet == 0)
+    {
+        printf("%d is not prime number \n",iValue);
+
+    }
+    else
+    {
+        printf("%d is  prime number\n",iValue);
+    }
+     return 0;
+}                                                                                                             
+
+
+
+
+
+
