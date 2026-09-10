69387b4f08779ac261da89426151aeaffd0be888
Chaitany Belambkar <chaitanybelambkar9689@gmail.com>
2026-09-10 17:46:23 +0530

    Add files via upload

---
+++ Programming-Practice/program56.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+bool CheckPrime(int iNo)
+{
+    int iCnt = 0;
+    
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+
+    }
+    for(iCnt=2;iCnt <= (iNo/2);iCnt++)
+    {
+        if((iNo %iCnt) ==0)
+        {
+            break;  //optimization due to break                            
+        }
+    }
+    if(iCnt > (iNo/2))
+    {                                                 //iNo=input
+        return true;
+    }
+    else  //Atleast one factor
+    {
+        return false;
+    }
+
+}
+int main()
+{
+    int iValue = 0;
+    bool bRet = false;
+
+    printf("Enter the number:\n");
+    scanf("%d",&iValue);
+    bRet =CheckPrime(iValue);
+    
+    if(bRet == 0)
+    {
+        printf("%d is not prime number \n",iValue);
+
+    }
+    else
+    {
+        printf("%d is  prime number\n",iValue);
+    }
+     return 0;
+}   
+
+/*
+Time complexity:for prime- N/2
+time complexity for non prtime:eithe 1 or 2
+
+
+*/
+
+
+
+
+
+
