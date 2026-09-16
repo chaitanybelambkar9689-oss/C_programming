11316b5f65e854b58b0036dfc53e3881584ab2c8
Chaitany Belambkar <chaitanybelambkar9689@gmail.com>
2026-09-16 23:28:56 +0530

    Add files via upload

---
+++ Programming-Practice/program62.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+int main()
+{
+    int iNo = 7236;
+    int iDigit = 0;
+    
+    printf("Original value of iNo is :%d\n",iNo);
+
+    printf("------------------------------------\n");
+    iDigit = iNo % 10 ;
+    printf("iDigit is: %d\n",iDigit);
+    printf("%d\n",iNo);
+    iNo = iNo/10; 
+
+    printf("------------------------------------\n");
+    iDigit = iNo % 10 ;
+    printf("iDigit is: %d\n",iDigit);
+    printf("%d\n",iNo);
+    iNo = iNo/10; 
+
+    printf("------------------------------------\n");
+    iDigit = iNo % 10 ;
+    printf("iDigit is: %d\n",iDigit);
+    printf("%d\n",iNo);
+    iNo = iNo/10; 
+
+    printf("------------------------------------\n");
+    iDigit = iNo % 10 ;
+    printf("iDigit is: %d\n",iDigit);
+ 
+    iNo = iNo/10; 
+    printf("iNo is:%d\n",iNo);
+
+    return 0;
+}
