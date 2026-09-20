29dd18e33bcd7746c14fc53ba27b754e43a8d438
Chaitany Belambkar <chaitanybelambkar9689@gmail.com>
2026-09-20 20:20:39 +0530

    Add files via upload

---
+++ Programming-Practice/program66.c
@@ -0,0 +1,31 @@
+// iteration
+
+#include<stdio.h>
+
+int main()
+{
+    int iNo = 0;
+    int iDigit = 0;
+   
+    printf("Enter  number:\n");
+    scanf("%d",&iNo);
+    
+    printf("------------------------------------\n");
+    printf("Original value of iNo is :%d\n",iNo);
+
+    while (iNo !=0) 
+    {
+      printf("------------------------------------\n");
+      iDigit = iNo % 10 ;
+      printf("iDigit is: %d\n",iDigit);
+      printf("%d\n",iNo);
+    iNo = iNo/10; 
+    }   
+
+   
+
+    printf("------------------------------------\n");
+   
+
+    return 0;
+}
